852f56d225589a1e88f07a17e318c5f7824d7243
Ruddy Rodriguez <ruddy.rodriguez.r@hotmail.com>
2023-04-27 08:52:20 -0500

    NT11 se crea archivo ruddy.py

---
+++ funcionRuddy.py
@@ -0,0 +1,5 @@
+booleano=True
+def clasificarCandidatos(numeroCarreras,numeroVictorias):
+    if ((numeroCarreras>100)&& (numeroCarreras = 10)):
+       booleano
+    
---
+++ funcionruddy.py
@@ -0,0 +1,5 @@
+
+def clasificarCandidatos(numeroCarreras,numeroVictorias):
+    if numeroCarreras>100&numeroCarreras == 10:
+     return True
+    
